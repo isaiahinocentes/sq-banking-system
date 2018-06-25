2b966fe252a4201412cc519b5cbcefdd523970f2
isaiahinocentes <isaiahpaulinocentes@gmail.com>
2018-06-25 20:40:43 +0800

    :Fix: Redirect to User after Login :Edit: My profile Page to show specific information

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -1,70 +1,51 @@
 @extends('layouts.header')
 @section('content')
     <div class="ui container" name="settings">
-        <h2 class="ui dividing header">Settings</h2>
+    
+        <h2 class="ui dividing header">Your Profile</h2>
     
         <table class="ui single line table">
             <thead>
                 <tr>
-                <th colspan="2">Change Password</th>
+                <th colspan="2">Personal Information</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <td>Old Password </td>
+                    <td>BALANCE</td>
                     <td>
-                        <input type="text" name="oldpass" value="" class="ui input" placeholder="Input Old Password">
+                        <h1> PHP {{ auth()->user()->balance }} </h1>
                     </td>
                 </tr>
                 <tr>
-                    <td>New Password </td>
+                    <td>ACCOUNT NUMBER</td>
                     <td>
-                        <input type="text" name="newpass" value="" class="ui input" placeholder="Input New Password">
+                        <h1> {{ auth()->user()->id }} </h1>
                     </td>
                 </tr>
                 <tr>
-                    <td>Confirm New Password </td>
+                    <td>Name </td>
                     <td>
-                        <input type="text" name="confirmpass" value="" class="ui input" placeholder="Confirm Password">
+                        {{ auth()->user()->name }}
                     </td>
                 </tr>
                 <tr>
-                    <input type="submit" name="changepass" value="Change Password" class="ui primary button">
+                    <td>Email</td>
+                    <td>
+                        {{ auth()->user()->email }}
+                    </td>
                 </tr>
-                
-            </tbody>
-        </table>
-    
-        <table class="ui single line table">
-            <thead>
                 <tr>
-                <th colspan="3">Information</th>
+                    <td>Alternate Email</td>
+                    <td>
+                        {{ auth()->user()->alt_email }}
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
                 <tr>
                     <td>Mobile Number</td>
-                    <td>123456789</td>
                     <td>
-                        <input type="text" name="newnumber" value="" class="ui input" placeholder="Input New Number">
+                        {{ auth()->user()->mobile }}
                     </td>
-                </tr>
-                <tr>
-                    <td>Email Address </td>
-                    <td>sampleemail@gmail.com</td>
-                    <td>
-                        <input type="text" name="newemail" value="" class="ui input" placeholder="Input New Email">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Alternate Email Address </td>
-                    <td>samplealternateemail@gmail.com</td>
-                    <td>
-                        <input type="text" name="newaltemail" value="" class="ui input" placeholder="Input New Alternate Email">
-                    </td>
-                </tr>
-                <tr>
-                    <input type="submit" name="changeinfo" value="Update" class="ui primary button">
                 </tr>
             </tbody>
         </table>
