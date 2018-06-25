ba2cf4cafcd9cc62d0f6bb2d383d0419842a46f6
isaiahinocentes <isaiahpaulinocentes@gmail.com>
2018-06-25 21:29:37 +0800

    :edit: transfer view :add: Logs migration and model and controller :todo: Finish backend apply singleton model for saving

---
+++ database/migrations/2018_06_25_125548_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('sender_id');
+            $table->unsignedInteger('recipient_id');
+            $table->string('transaction_type');
+            $table->float('amount');
+            $table->string('remarks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
--- resources/views/user/transfer.blade.php
+++ resources/views/user/transfer.blade.php
@@ -2,114 +2,64 @@
 
 @section('content')
     <div class="container">
-        <div class="ui container" name="fundtransfer">
-        <h2 class="ui dividing header">Fund Transfer</h2>
-        <table class="ui single line table">
-            <thead>
-                <tr>
-                <th colspan="3">Transfer Details</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Transfer From </td>
-                    <td>
-                        <select class="ui dropdown">
-                            <option value="">Choose One</option>
-                            <option value="1234567891234">123456789123(SA-PHP-Living Expenses)</option>
-                        </select>
-                    </td>
-                    <td><a>View Balance</a></td>
-                </tr>
-                <tr>
-                    <td>Transfer To </td>
-                    <td>
-                        <select class="ui dropdown">
-                            <option value="">Choose One</option>
-                            <option value="345678912345">345678912345</option>
-                            <option value="input">Input Account No. </option>
-                        </select>
-                    </td>
-                    <td>
-                        <input type="text" name="accountnum" value="" class="ui input" placeholder="Input Account Number">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Amount</td>
-                    <td>
-                        <input type="text" name="amount" value="" class="ui input" placeholder="Input Amount">
-                    </td>
-                </tr>
-                <tr>
-                    <td>Remarks</td>
-                    <td>
-                        <input type="text" name="remarks" value="" class="ui input" placeholder="Comments">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <form>
+            @csrf
+            <div class="ui container" name="fundtransfer">
+                <h2 class="ui dividing header">Fund Transfer</h2>
+                <table class="ui single line table">
+                    <thead>
+                        <tr>
+                            <th colspan="3">Transfer Details</th>
+                        </tr>
+                    </thead>
+                    {{-- Table Body --}}
+                    <tbody>
+                        {{-- Balance --}}
+                        <tr>
+                            <td>BALANCE</td>
+                            <td colspan="2">
+                                <h1>
+                                    PHP {{ auth()->user()->balance}}
+                                </h1>
+                            </td>
+                        </tr>
+                        {{-- Account Number --}}
+                        <tr>
+                            <td>Transfer To </td>
+                            <td colspan="2">
+                                <input type="text" name="account_num" value="" class="ui input" placeholder="Input Account Number">
+                            </td>
+                        </tr>
+                        {{-- Amount --}}
+                        <tr>
+                            <td>Amount</td>
+                            <td>
+                                <input type="text" name="amount" value="" class="ui input" placeholder="Input Amount">
+                            </td>
+                        </tr>
+                        {{-- Remarks --}}
+                        <tr>
+                            <td>Remarks</td>
+                            <td>
+                                <input type="text" name="remark" value="" class="ui input" placeholder="Comments">
+                            </td>
+                        </tr>
 
+                    </tbody>
+                </table>
 
-        <table class="ui single line table">
-            <thead>
-                <tr>
-                <th colspan="3">Transfer Schedule</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="radio" checked="checked">
-                            <label>Immediately</label>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="ui radio checkbox">
-                            <input type="radio" name="radio" checked="checked">
-                            <label>Later Date</label>
-                        </div>
-                    </td>
-                    <td>
-                        <input type="date" name="date" value="" class="ui input">
-                    </td>
-                </tr>
-                
-            </tbody>
-        </table>
-
-        <table class="ui single line table">
-            <thead>
-                <tr>
-                    <th colspan="2">Add To My Transfers</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>
-                        Save to My Transfers As
-                    </td>
-                    <td>
-                        <input type="text" name="date" value="" class="ui input" placeholder="Input Name">
-                    </td>
-                </tr>
-                
-            </tbody>
-        </table>
-
-        <br>
-        <br>
-
-        <center>
-            <input type="submit" name="transfer" value="Transfer Money" class="ui primary button">
-        </center>
+                <br>
+                <br>
+        
+                <center>
+                    <input type="submit" name="transfer" value="Transfer Money" class="ui primary button">
+                </center>
+            </div>
+        </form>
     </div>
-    <br>
-    <br>
-    <br>
 
     <br>
     <br>
     <br>
-    </div>
+    
 @endsection
